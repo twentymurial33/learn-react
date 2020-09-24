f28a96efd4373825eff955bf09f03815fcc92328
Murial <murial@murials-air.myfiosgateway.com>
2020-09-24 11:53:05 -0400

    added main bar and react-mdl

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import {Header,Navigation,}from 'react-mdl';
+import Main from './Components/main';
+import {Link} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+class App extends Component{
+  render(){
+    return(
+      <div className="Murial's-portfolio">
+        
+          <Header className="header-color" to="/">
+          <Navigation>
+
+          </Navigation>
+            </Header> 
+      
+      </div>
+
+    )
+
+    
+
+  }
 }
+
 
 export default App;
