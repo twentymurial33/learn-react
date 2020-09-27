e1fe20f8c55539fb3ec0cdd5190e8ff22bbdebed
Murial <murial@murials-air.myfiosgateway.com>
2020-09-27 17:15:21 -0400

    new changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Header,Navigation,}from 'react-mdl';
+import {Header,Navigation,Layout,Drawer}from 'react-mdl';
 import Main from './Components/main';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
 
 
 class App extends Component{
   render(){
     return(
       <div className="Murial's-portfolio">
-        
-          <Header className="header-color" to="/">
+        <Layout>
+          <Header className="header-color"title={<Link style={{textDecoration:'none',color:'orange'}} to='/'> MURIAL'S PORTFOLIO</Link>} scroll>
           <Navigation>
-
+            <Link to="/resume">Resume</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/contacts">Contact</Link>
           </Navigation>
             </Header> 
-      
+            <Drawer title ={<Link style={{textDecoration:'none',color:'black'}} to="/">MURIAL'S PORTFOLIO</Link>}>
+              <Navigation>
+              <Link to="/resume">Resume</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/contacts">Contact</Link>
+              </Navigation>
+            </Drawer>
+      </Layout>
+      <Main/>
       </div>
 
     )
---
+++ src/Components/aboutme.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+class about extends Component{
+    render(){
+        return(
+            <div><h5>A proven record of leading complex and diverse technology programs and projects that drive organizational tranformations and growth.
+                Solid understanding of cloud technologoes and concepts with experience in sofware development and product delivery</h5></div>
+        )
+    }
+}
+
+export default about;
